26733948556447d7ed3b2f176753236ef7ab60a9
Zarraq <37717216+zARRAQ@users.noreply.github.com>
2026-05-20 13:56:26 +0300

    Delete Fedora script -basic directory

--- Fedora script -basic/2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "turning surface kernel into defult kernal"
-sudo systemctl enable --now linux-surface-default-watchdog.path
-sudo systemd enable --now linux-surface-default-watchdog.path
-sudo linux-surface-default-watchdog.py
-echo "Finally, reboot your system and you should boot into the linux-surface kernel. Please make sure you are actually using the right kernel by checking if the output of uname -a contains the string surface. If it doesn't contain that string, you are still using the default kernel."
